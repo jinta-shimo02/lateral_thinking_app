c3990d761d2a7653d34c5a894d9d2f98e5a0ee1d
KeiSAKATA <kei_sakata@nexway.co.jp>
2024-07-20 15:50:09 +0900

    gemini ai result to text

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,25 @@
 class QuestionsController < ApplicationController
+
   def home
+    # With an API key
+    client = Gemini.new(
+      credentials: {
+        service: 'generative-language-api',
+        api_key: ENV['GOOGLE_API_KEY']
+      },
+      options: { model: 'gemini-pro', server_sent_events: true }
+    )
+
+    result = client.stream_generate_content(
+      { contents: { role: 'user', parts: { text: 'おはようございます、本日の天気を教えていただけないでしょうか' } } }
+    )
+
+    data = result.first
+
+    # data = JSON.parse(result.first)
+
+    text = data['candidates'][0]['content']['parts'][0]['text']
+
+    puts text
   end
 end
