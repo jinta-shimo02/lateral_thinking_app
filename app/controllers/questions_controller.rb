5cf8f89d291f00603d8fe41c0b9e3016626a6572
jinta-shimo02 <jinjinnbaaahi.0514@icloud.com>
2024-07-21 13:22:49 +0900

    プロンプトの精度をあげた

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,4 @@
 class QuestionsController < ApplicationController
-
   def home
     $random_question = Question.order('RANDOM()').first
     $random_answer = $random_question.answers.order('RANDOM()').first
--- app/services/gemini_service.rb
+++ app/services/gemini_service.rb
@@ -9,29 +9,9 @@ class GeminiService
     )
   end
 
-  # def set_questions_and_answers(questions, answers)
-  #   @questions = questions
-  #   @answers = answers
-  # end
-
   def get_response(message)
     result = @client.stream_generate_content(
-      { contents: { role: 'user', parts: { text: """
-      あなたは、クリティカルシンキングを教えるプロの講師です。
-      今回、受講生に対して、水平思考（lateral thinking）の力をクイズを通して養ってもらうことにしました。
-
-      ルールは下記です。
-
-      あなた（講師）は「お題」に対して生徒から質問を受け付けます。
-      生徒からの質問が「正解」とほとんど同じだった場合、「正解です」と答えたのち、既成の理論や概念にとらわれずアイデアを生み出すことができたかどうかフィードバックを行います。
-      生徒からの質問が「正解」と違う場合は、生徒からの質問に対してあなた（講師）は正解と生徒からの質問内容を照らし合わせて考えて「はい」または「いいえ」、「どちらでもよい」の３種類のみで答えます。
-      ※「はい」は、明確に正解と因果関係がある場合、「いいえ」は明確に正解と因果関係がない場合、「どちらでもよい」は「はい」にも「いいえ」にも当てはまらない場合に使います。
-
-      お題と正解は下記です。
-      お題：#{$random_question.content}
-      正解：#{$random_answer.content}
-      生徒からの質問：
-      """+ message } } }
+      { contents: { role: 'user', parts: { text: "#{pronpt($random_question, $random_answer)}"+ message } } }
     )
     data = result.first
     text = data['candidates'][0]['content']['parts'][0]['text']
@@ -40,4 +20,28 @@ class GeminiService
     Rails.logger.error("Error fetching response from Gemini: #{e.message}")
     'エラーが発生しました。後でもう一度試してください。'
   end
+end
+
+def pronpt(question, answer)
+  text = <<~EOS
+    今から水平思考クイズを開催いたします。
+    水平思考クイズとは、別名「ウミガメのスープ」と言われていて、出題者が出した問題に対して、回答者が質問を繰り返し、解答を導き出すクイズのことです。
+
+    あなたは、水平思考クイズの出題者です。
+    問題と解答は以下のとおりです。
+
+    問題：#{question.content}
+    解答：#{answer.content}
+
+    回答者から質問が来ますので、以下のルールで答えてください。
+
+    ルールは以下のとおりです。
+    
+    ・回答者からの質問が「解答」と違う場合は、「解答」と「質問」を比較して、「はい」「いいえ」「関係ありません」と答えてください。
+    ・回答者からの質問が「解答」と同じような意味の場合は、「正解です！」と答えてください。
+    ・回答者からの質問に、「解答」と同じ単語が含まれていた場合は、「正解です！」と答えてください。
+
+    回答者からの質問：
+  EOS
+  text
 end
