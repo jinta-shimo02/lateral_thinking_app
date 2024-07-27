99c98a843aa7fbf448d834fc7ad43f5f5f1015d3
jinta-shimo02 <jinjinnbaaahi.0514@icloud.com>
2024-07-27 12:23:38 +0900

    問題解答データにレベルを追加

--- lib/questions/question_save.rb
+++ lib/questions/question_save.rb
@@ -3,13 +3,21 @@ require 'csv'
 CSV.foreach("lib/questions/question.csv", headers: true) do |row|
   count = row.count
   question = Question.find_by(content: row[1])
+  level = case row[count - 1]
+          when "easy"
+            :easy
+          when "normal"
+            :normal
+          when "difficult"
+            :difficult
+          end
 
   if question.nil?
-    new_question = Question.create(content: row[1])
+    new_question = Question.create(content: row[1], level:)
   end
 
   if new_question.present?
-    (2..count).each do |i|
+    (2..(count - 1)).each do |i|
       unless row[i].nil?
         Answer.create(content: row[i], question_id: new_question.id)
       end
